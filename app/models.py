7fbb5d0f34609bfd63a17a128e549c265b1d679e
David Malcolm <david.a.malcolms@gmail.com>
2024-04-02 02:25:40 -0500

    created movie model and api route form

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
 # Add any model classes for Flask-SQLAlchemy here
+from app import db
+
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
--- app/views.py
+++ app/views.py
@@ -5,23 +5,56 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request, jsonify, send_file
 import os
 
+from flask import jsonify, render_template, request, send_file
+
+from app import app
+from app.forms import MovieForm
+from app.models import Movie
 
 ###
 # Routing for your application.
 ###
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return jsonify(message="This is the beginning of our API")
+
+
+@app.route("/api/v1/movies", methods=["POST"])
+def movies():
+    form = MovieForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
+        poster = request.files["poster"]
+        poster_filename = secure_filename(poster.filename)
+        poster.save(os.path.join(app.config["UPLOAD_FOLDER"], poster_filename))
+
+        # Create new movie instance
+        new_movie = Movie(title=title, description=description, poster=poster_filename)
+        db.session.add(new_movie)
+        db.session.commit()
+
+        return jsonify(
+            {
+                "message": "Movie Successfully added",
+                "title": new_movie.title,
+                "poster": new_movie.poster,
+                "description": new_movie.description,
+            }
+        )
+    else:
+        errors = form_errors(form)
+        return jsonify({"errors": errors}), 400
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
 
 # Here we define a function to collect form errors from Flask-WTF
 # which we can later use
@@ -30,18 +63,19 @@ def form_errors(form):
     """Collects form errors"""
     for field, errors in form.errors.items():
         for error in errors:
-            message = u"Error in the %s field - %s" % (
-                    getattr(form, field).label.text,
-                    error
-                )
+            message = "Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error,
+            )
             error_messages.append(message)
 
     return error_messages
 
-@app.route('/<file_name>.txt')
+
+@app.route("/<file_name>.txt")
 def send_text_file(file_name):
     """Send your static text file."""
-    file_dot_text = file_name + '.txt'
+    file_dot_text = file_name + ".txt"
     return app.send_static_file(file_dot_text)
 
 
@@ -52,12 +86,13 @@ def add_header(response):
     and also tell the browser not to cache the rendered page. If we wanted
     to we could change max-age to 600 seconds which would be 10 minutes.
     """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
+    response.headers["X-UA-Compatible"] = "IE=Edge,chrome=1"
+    response.headers["Cache-Control"] = "public, max-age=0"
     return response
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
